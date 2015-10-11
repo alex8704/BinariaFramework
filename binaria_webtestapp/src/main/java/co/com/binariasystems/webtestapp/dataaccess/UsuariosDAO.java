5617a57666cbe9cc83abd65a3cea752a9b119ff2
Alexander Castro O <alexander_8704@yahoo.com.co>
2015-10-11 18:11:45 -0500

    Spring Data JPA

---
+++ binaria_webtestapp/src/main/java/co/com/binariasystems/webtestapp/dataaccess/UsuariosDAO.java
@@ -0,0 +1,8 @@
+package co.com.binariasystems.webtestapp.dataaccess;
+
+import co.com.binariasystems.fmw.dataaccess.JPABasedDAO;
+import co.com.binariasystems.webtestapp.entity.Usuario;
+
+public interface UsuariosDAO extends JPABasedDAO<Usuario, Long>{
+	
+}
--- binaria_webtestapp/src/main/java/co/com/binariasystems/webtestapp/entity/Usuario.java
+++ binaria_webtestapp/src/main/java/co/com/binariasystems/webtestapp/entity/Usuario.java
@@ -1,10 +1,49 @@
 package co.com.binariasystems.webtestapp.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="usuarios")
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_usuario")
+	private Long id;
+	@Column(name="nombre_usuario")
+	private String alias;
+	@Column(name="password")
+	private String contrasenia;
+	@Column(name="password_salt")
+	private String saltContrasenia;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getAlias() {
+		return alias;
+	}
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+	public String getSaltContrasenia() {
+		return saltContrasenia;
+	}
+	public void setSaltContrasenia(String saltContrasenia) {
+		this.saltContrasenia = saltContrasenia;
+	}
+	
 	
 }
