97fd797b536e637e697b0c1828724036595c4e15
Alexander Castro O <alexander_8704@yahoo.com.co>
2015-10-12 14:14:05 -0500

    Modelgen

--- binaria_webtestapp/src/main/java/co/com/binariasystems/webtestapp/business/AuthenticationBusiness.java
+++ binaria_webtestapp/src/main/java/co/com/binariasystems/webtestapp/business/AuthenticationBusiness.java
@@ -1,10 +1,13 @@
 package co.com.binariasystems.webtestapp.business;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import co.com.binariasystems.fmw.business.FMWBusiness;
+import co.com.binariasystems.webtestapp.dataaccess.UsuariosDAO;
+import co.com.binariasystems.webtestapp.entity.Usuario;
 
 public interface AuthenticationBusiness extends FMWBusiness {
 	
@@ -16,8 +19,17 @@ public interface AuthenticationBusiness extends FMWBusiness {
 			propagation=Propagation.SUPPORTS, readOnly=true
 			)
 	public static class AuthenticationBusinessImpl implements AuthenticationBusiness{
+		@Autowired
+		private UsuariosDAO dao;
+		
+		
 		@Override
 		public String dato() {
+			Iterable<Usuario> users = dao.findAll();
+			System.out.println("{USUARIOS DE LA BASE DE DATOS}");
+			for(Usuario u : users){
+				System.out.println("Usuario: "+u.getAlias()+", Contrasenia: "+u.getContrasenia());
+			}
 			return "{Este es el DATO gilipollete}";
 		}
 	}
