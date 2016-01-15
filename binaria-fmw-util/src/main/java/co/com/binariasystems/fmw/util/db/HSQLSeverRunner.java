52508625743d67c4fba4ef67ce042dc28f49b866
Alexander Castro Olivo <alexander.castro@techandsolve.com>
2016-01-15 17:18:02 -0500

    nuevas clases para ejecutar en ambiente de desarrollo un servidor HSQL directamente desde la aplicacion

---
+++ binaria-fmw-util/src/main/java/co/com/binariasystems/fmw/util/db/HSQLSeverRunner.java
@@ -0,0 +1,125 @@
+package co.com.binariasystems.fmw.util.db;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.lang3.StringUtils;
+import org.hsqldb.persist.HsqlProperties;
+import org.hsqldb.server.Server;
+import org.hsqldb.server.ServerAcl.AclFormatException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HSQLSeverRunner{
+	private static final Logger LOGGER = LoggerFactory.getLogger(HSQLSeverRunner.class);
+	private static final String DEFAULT_DIRECTORY = System.getProperty("user.home");
+	private Properties serverConfig;
+	private static final int DEFAULT_PORT = 9001;
+	private String dataBasesDirectory;
+	private List<String> databaseNames;
+	private int serverPort;
+	private Server server;
+	private boolean serverStarted;
+	
+	public void startServer(){
+		if(serverStarted) return;
+		if(serverConfig == null)buildServerConfig();;
+		HsqlProperties serverProps = new HsqlProperties(serverConfig);
+		server = new Server();
+		try {
+			server.setProperties(serverProps);
+		} catch (IOException | AclFormatException e) {
+			LOGGER.error("Error starting HSQL Server", e);
+			return;
+		}
+		server.start();
+		serverStarted = !serverStarted;
+	}
+	
+	
+	private void buildServerConfig(){
+		dataBasesDirectory = StringUtils.defaultIfBlank(dataBasesDirectory, DEFAULT_DIRECTORY);
+		serverConfig = new Properties();
+		serverConfig.setProperty("server.no_system_exit", "true");
+		serverConfig.setProperty("server.port", String.valueOf((serverPort <= 0) ? DEFAULT_PORT : serverPort));
+		int dbIdx = 0;
+		for(String  dbName : databaseNames){
+			serverConfig.setProperty("server.database."+(dbIdx++), "file:"+dataBasesDirectory+"/"+dbName+"/db");
+			serverConfig.setProperty("server.dbname."+(dbIdx++), dbName);
+		}
+		
+	}
+	
+	public void stopServer(){
+		if(!serverStarted)return;
+		server.shutdown();
+		serverStarted = !serverStarted;
+	}
+
+
+	/**
+	 * @return the serverConfig
+	 */
+	public Properties getServerConfig() {
+		return serverConfig;
+	}
+
+
+	/**
+	 * @param serverConfig the serverConfig to set
+	 */
+	public void setServerConfig(Properties serverConfig) {
+		this.serverConfig = serverConfig;
+	}
+
+
+	/**
+	 * @return the dataBasesDirectory
+	 */
+	public String getDataBasesDirectory() {
+		return dataBasesDirectory;
+	}
+
+
+	/**
+	 * @param dataBasesDirectory the dataBasesDirectory to set
+	 */
+	public void setDataBasesDirectory(String dataBasesDirectory) {
+		this.dataBasesDirectory = dataBasesDirectory;
+	}
+
+
+	/**
+	 * @return the databaseNames
+	 */
+	public List<String> getDatabaseNames() {
+		return databaseNames;
+	}
+
+
+	/**
+	 * @param databaseNames the databaseNames to set
+	 */
+	public void setDatabaseNames(List<String> databaseNames) {
+		this.databaseNames = databaseNames;
+	}
+
+
+	/**
+	 * @return the serverPort
+	 */
+	public int getServerPort() {
+		return serverPort;
+	}
+
+
+	/**
+	 * @param serverPort the serverPort to set
+	 */
+	public void setServerPort(int serverPort) {
+		this.serverPort = serverPort;
+	}
+	
+	
+}
---
+++ binaria-fmw-vweb/src/main/java/co/com/binariasystems/fmw/vweb/servlet/HSQLSeverRunListener.java
@@ -0,0 +1,70 @@
+package co.com.binariasystems.fmw.vweb.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.StringTokenizer;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import co.com.binariasystems.fmw.util.db.HSQLSeverRunner;
+
+public class HSQLSeverRunListener implements ServletContextListener {
+	private static final Logger LOGGER = LoggerFactory.getLogger(HSQLSeverRunListener.class);
+	public static final String BASE_DIRECTORY = "hsqlRunner.dataBasesDirectory";
+	public static final String DATABASE_NAMES = "hsqlRunner.databaseNames";
+	public static final String SERVER_PORT = "hsqlRunner.serverPort";
+	public static final String SERVER_CONFIG_FILE = "hsqlRunner.serverConfigFile";
+	private HSQLSeverRunner serverRunner;
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		if(serverRunner != null)
+			serverRunner.stopServer();
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		String configFileParam = sce.getServletContext().getInitParameter(SERVER_CONFIG_FILE);
+		serverRunner = new HSQLSeverRunner();
+		if(StringUtils.isBlank(configFileParam)){
+			serverRunner.setDataBasesDirectory(sce.getServletContext().getInitParameter(BASE_DIRECTORY));
+			serverRunner.setServerPort(Integer.parseInt(StringUtils.defaultIfBlank(sce.getServletContext().getInitParameter(SERVER_PORT), "0")));
+			String databaseNamesParam = StringUtils.defaultString(sce.getServletContext().getInitParameter(DATABASE_NAMES));
+			StringTokenizer dbNamesTokenizer = new StringTokenizer(databaseNamesParam, ",");
+			List<String> dbNames = (dbNamesTokenizer.countTokens() > 0) ? new ArrayList<String>() : null;
+			while(dbNamesTokenizer.hasMoreTokens()){
+				dbNames.add(dbNamesTokenizer.nextToken());
+			}
+			serverRunner.setDatabaseNames(dbNames);
+		}else{
+			Properties serverConfig = new Properties();
+			try{
+				if(configFileParam.endsWith(".xml"))
+					serverConfig.loadFromXML(HSQLSeverRunListener.class.getResourceAsStream(configFileParam));
+				else
+					serverConfig.load(HSQLSeverRunListener.class.getResourceAsStream(configFileParam));
+				serverRunner.setServerConfig(serverConfig);
+			}catch(IOException ex){
+				LOGGER.error("Has ocurred an configuring HSQL Server", ex);
+				return;
+			}
+		}
+		
+		serverRunner.startServer();
+		waitServerStart();
+	}
+	
+	private void waitServerStart(){
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
